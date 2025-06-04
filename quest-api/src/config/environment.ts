fb62798334f6133a46dbad45626008f7d03ae8c7
Mark Zhuravel <olma75830717@gmail.com>
2025-06-04 13:57:43 +0200

    fix: добавлен localhost:5174 в CORS

--- quest-api/src/config/environment.ts
+++ quest-api/src/config/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   isDevelopment: process.env.NODE_ENV !== 'production',
   frontend: {
     urls: process.env.NODE_ENV === 'production'
-      ? ['https://escape-room-eight-phi.vercel.app', 'https://escape-room-eight-phi.vercel.app/api']
-      : ['http://localhost:5173', 'http://localhost:3000']
+      ? ['https://escape-room-eight-phi.vercel.app']
+      : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000']
   },
   database: {
     postgresql: process.env.DATABASE_URL,
